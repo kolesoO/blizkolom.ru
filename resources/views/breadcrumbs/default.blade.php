49c5bd7295a4fac49b8fd3bf6d086c3b11e8410e
kDevelop <kat-kole@mail.ru>
2020-06-09 11:24:01 +0300

    breadcrumbs fixes

--- resources/views/breadcrumbs/default.blade.php
+++ resources/views/breadcrumbs/default.blade.php
@@ -5,17 +5,24 @@
     $fullCount = count($breadcrumbs);
 @endphp
 
-<div class="bread" itemscope="" itemtype="https://schema.org/BreadcrumbList">
-    @foreach ($breadcrumbs as $key => $breadcrumb)
-        <span itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-            <a
-                    itemprop="item"
-                    href="{{ $breadcrumb->url }}"
-            >{{ $breadcrumb->title }}<meta itemprop="name" content="{{ $breadcrumb->meta_content ?? '' }}"></a>
-            @if ($key < $fullCount - 1)
-                <span> / </span>
-            @endif
-            <meta itemprop="position" content="{{ ($key + 1) }}">
-        </span>
-    @endforeach
-</div>
+@if ($fullCount > 0)
+    <div class="bread" itemscope="" itemtype="https://schema.org/BreadcrumbList">
+        @foreach ($breadcrumbs as $key => $breadcrumb)
+            <span itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                @if ($key < $fullCount - 1)
+                    <a
+                            itemprop="item"
+                            href="{{ $breadcrumb->url }}"
+                    >{{ $breadcrumb->title }}<meta itemprop="name" content="{{ $breadcrumb->meta_content ?? '' }}"></a>
+                    <span> / </span>
+                @else
+                    <span
+                            itemprop="item"
+                            content="{{ $breadcrumb->url }}"
+                    >{{ $breadcrumb->title }}<meta itemprop="name" content="{{ $breadcrumb->meta_content ?? '' }}"></span>
+                @endif
+                <meta itemprop="position" content="{{ ($key + 1) }}">
+            </span>
+        @endforeach
+    </div>
+@endif
