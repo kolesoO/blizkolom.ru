f925760cd623b05be7057588930c58b0a637b56c
kDevelop <kat-kole@mail.ru>
2020-09-14 21:43:39 +0300

    add rating widget for search systems

--- resources/views/pages/desktop/index.blade.php
+++ resources/views/pages/desktop/index.blade.php
@@ -8,7 +8,32 @@
         <v-map></v-map>
         <div id="cards">{!! $company_list !!}</div>
     </div>
+
     {{ \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render() }}
+
+    <script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "Product",
+      "name": "{{ $header["seo"]["h1"] }}",
+      "description": "{{ $header["seo"]["description"] }}",
+      "AggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": "9.8",
+        "reviewCount": "{{ (int) $header['company_count']['count'] * 2 }}",
+        "bestRating": "10"
+      },
+      "image": {
+        "@type": "ImageObject",
+        "url": "https://static.blizkolom.ru/img/lp/punkt-priema-410.jpg"
+      },
+      "offers": {
+        "@type": "AggregateOffer",
+        "lowPrice": "0",
+        "priceCurrency": "RUB"
+      }
+    }
+    </script>
 </div>
 
 @include("site-templates.public.desktop.footer")
--- resources/views/pages/mobile/index.blade.php
+++ resources/views/pages/mobile/index.blade.php
@@ -5,6 +5,31 @@
 </div>
 <v-map></v-map>
 <div class="cards">{!! $company_list !!}</div>
+
 {{ \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render() }}
+
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "Product",
+  "name": "{{ $header["seo"]["h1"] }}",
+  "description": "{{ $header["seo"]["description"] }}",
+  "AggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "9.8",
+    "reviewCount": "{{ (int) $header['company_count']['count'] * 2 }}",
+    "bestRating": "10"
+  },
+  "image": {
+    "@type": "ImageObject",
+    "url": "https://static.blizkolom.ru/img/lp/punkt-priema-410.jpg"
+  },
+  "offers": {
+    "@type": "AggregateOffer",
+    "lowPrice": "0",
+    "priceCurrency": "RUB"
+  }
+}
+</script>
 
 @include("site-templates.public.mobile.footer")
